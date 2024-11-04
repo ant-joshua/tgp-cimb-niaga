1536b592a8d36f89538e2d194a62c37f08d78e8d
Antonius Joshua <antoniusjoshua47@gmail.com>
2024-11-04 09:19:21 +0700

    feat: add find es6

--- latihan12/scripts/index.js
+++ latihan12/scripts/index.js
@@ -31,3 +31,35 @@ function filterOddES6() {
   const numbers = [1, 60, 112, 123, 100, 99, 73];
   return numbers.filter((number) => number % 2 !== 0);
 }
+
+// Find ES6
+function findStudent(id) {
+  const studentList = [
+    {
+      id: 1,
+      name: "John Doe",
+      score: 90,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      score: 95,
+    },
+    {
+      id: 3,
+      name: "Michael Doe",
+      score: 98,
+    },
+  ];
+
+  return studentList.find((student) => student.id === id);
+}
+
+/**
+ * findStudent(1)
+ * {
+      id: 1,
+      name: "John Doe",
+      score: 90,
+    },
+ */
