ad1c9dcf34d7fc85cc3a52bb519d703faf1b0ef9
Antonius Joshua <antoniusjoshua47@gmail.com>
2024-11-08 14:15:14 +0700

    feat: update example

--- latihan20/tests/example.spec.ts
+++ latihan20/tests/example.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from "@playwright/test";
 
+const awaitTimeout = (delay) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
+
 test("Testing Input Username", async ({ page }) => {
   await page.goto("https://trx-practices-h8.vercel.app/");
 
@@ -11,6 +14,14 @@ test("Testing Input Username", async ({ page }) => {
 
   await page.waitForSelector("#login-btn");
   await page.click("#login-btn");
+
+  await page.waitForSelector("#amount");
+  await page.fill("#amount", "100000");
+
+  await page.waitForSelector("#add-transaction-btn");
+  await page.click("#add-transaction-btn");
+
+  await page.waitForSelector("#transaction-list");
 
   await page.close();
 });
